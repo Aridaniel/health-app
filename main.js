a23dc52c35331ace32b82dc913916bcd0c5381b7
Johann <johannth88@gmail.com>
2020-10-16 12:25:07 +0000

    calendar fixed and next and previous month buttons added

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
-console.log("helo");
+// All months in Icelandic to display in the calendar
+const allMonths = ['Janúar', 'Febrúar', 'Mars', 'Apríl', 'Maí', 'Júní', 'Júlí', 'Ágúst', 'September', 'Október', 'Nóvember', 'Desember'];
 
+// DOM element variables
+const calendarElement = document.getElementById('calendar');
+const currentMonthElement = document.getElementById('current-month');
+const currentYearElement = document.getElementById('current-year');
+const prevMonthElement = document.getElementById('prev-month');
+const nextMonthElement = document.getElementById('next-month');
+
+// Initialize today as the date today
+let today = new Date();
+
+// Add event listeners once the DOM content is loaded
+document.addEventListener('DOMContentLoaded', () => {
+    prevMonthElement.addEventListener('click', changeMonth);
+    nextMonthElement.addEventListener('click', changeMonth);
+});
+
+// Call this each time the next or previous month button is clicked
+function changeMonth(ev) {
+    if(ev.currentTarget.id === 'prev-month') {
+        today = new Date(today.getFullYear(), today.getMonth() - 1);
+        let newMonth = getDaysInMonth(today.getMonth(), today.getFullYear());
+        displayMonthInCalendar(newMonth);
+    }
+    if(ev.currentTarget.id === 'next-month') {
+        console.log('Today before change: ' + today);
+        today = new Date(today.getFullYear(), today.getMonth() + 1);
+        console.log('today after change: ' + today);
+        let newMonth = getDaysInMonth(today.getMonth(), today.getFullYear());
+        displayMonthInCalendar(newMonth);
+    }
+}
+
+// Returns an array of every day in given month and year as date objects
 function getDaysInMonth(month, year) {
     var date = new Date(year, month, 1);
     var days = [];
@@ -10,21 +44,41 @@ function getDaysInMonth(month, year) {
     return days;
 }
 
-const calendarElement = document.getElementById('calendar');
-let daysInSeptember = getDaysInMonth(3, 2020);
+// Remove every element that has the .date class from the site
+function clearCalendar() {
+    const datesArray = document.querySelectorAll('.date');
+    for(let i = 0; i < datesArray.length; i++) {
+        datesArray[i].remove();
+    }
+}
 
+// Pass in an array of date objects in a month and display them in the calendar
 function displayMonthInCalendar(month) {
+    clearCalendar();
+    let firstDayIndex = month[0].getDay();
     let currentDay = 0;
-    for(let i = 0; i < 35; i++) {
+    currentMonthElement.innerHTML = allMonths[month[0].getMonth()];
+    currentYearElement.innerHTML = month[0].getFullYear();
+    // Create 42 div elements (7 columns, 6 rows)
+    for(let i = 0; i < 42; i++) {
         let dayElement = document.createElement('div');
-        if(i >= month[currentDay].getDay()) {
+        dayElement.classList.add('date');
+        // If i is equal or greater then firstDayIndex we know we are positioned on the correct weekday and we can start marking the dates
+        if(i >= firstDayIndex && currentDay < month.length) {
             dayElement.innerHTML = month[currentDay].getDate();
+            // Incrementing currentDay to access the next day of the month array
             currentDay++;
         }
         calendarElement.appendChild(dayElement);
     }
 }
 
-console.log(daysInSeptember[0].getDay());
+// Initialize the calendar with today as the month to display
+let daysInCurrentMonth = getDaysInMonth(today.getMonth(), today.getFullYear());
+displayMonthInCalendar(daysInCurrentMonth);
 
-displayMonthInCalendar(daysInSeptember);
+// ToDo: 
+// highlighta current day
+// gera all daga clickable
+// fela tóma reyti
+// Stíla 
